d50e57d0fd027752c51475dbfe38dbcbe2377142
Parth Sinha <sinhaparth555@gmail.com>
2025-01-11 17:08:32 +0000

    updated tailwind css for animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      blur: {
+        xs: '1px',
+      },
       fontFamily: {
         sans: [
           "DM Sans",
